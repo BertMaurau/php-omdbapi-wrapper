13d1ae4cfcc5324a5106608f13823a09a5ad6e76
Bert Maurau <hello@bertmaurau.be>
2018-08-14 23:24:12 +0200

    split info between brackets from name as extra

--- examples/getMovie.php
+++ examples/getMovie.php
@@ -5,10 +5,9 @@ require_once __DIR__ . "/../src/loader.php";
 $api = new BertMaurau\OMDB\Core\API("3077118c");
 
 
-// $title = (new BertMaurau\OMDB\Models\Title) -> getByImdbId("tt2085059");
+$title = (new BertMaurau\OMDB\Models\Title) -> getByImdbId("tt3896198");
 
-$title = (new BertMaurau\OMDB\Models\Title) -> getByTitle("Total Recall", 1990, "series");
-
+// $title = (new BertMaurau\OMDB\Models\Title) -> getByTitle("Total Recall", 1990, "movies");
 // check if it has response
 if ($title -> response) {
     // get data
--- src/BertMaurau/OMDB/Models/Person.php
+++ src/BertMaurau/OMDB/Models/Person.php
@@ -12,9 +12,18 @@ class Person
 
     // string
     private $name;
+    private $extra;
 
     public function __construct($name)
     {
+        // check for extra info between brackets
+        preg_match('#\((.*?)\)#', $name, $extra);
+
+        if (count($extra) > 0) {
+            // remove that part from the name
+            $name = trim(str_replace($extra[0], "", $name));
+            $this -> setExtra($extra[1]);
+        }
         $this -> setName($name);
     }
 
@@ -27,6 +36,11 @@ class Person
         return $this -> name;
     }
 
+    public function getExtra()
+    {
+        return $this -> extra;
+    }
+
     /**
      * Set the person's name
      * @param string $name
@@ -35,6 +49,17 @@ class Person
     public function setName($name)
     {
         $this -> name = (string) $name;
+        return $this;
+    }
+
+    /**
+     * Set the extra info
+     * @param string $extra
+     * @return $this
+     */
+    public function setExtra($extra)
+    {
+        $this -> extra = (string) $extra;
         return $this;
     }
 
