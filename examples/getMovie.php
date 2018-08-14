17db9b89fdfd3bc1727cf35e259f6c0a859586b0
Bert Maurau <hello@bertmaurau.be>
2018-08-14 22:15:16 +0200

    added examples

---
+++ examples/getMovie.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/../src/loader.php";
+
+$api = new BertMaurau\OMDB\Core\API("3077118c");
+
+
+$title = (new BertMaurau\OMDB\Models\Title) -> getByImdbId("tt2085059");
+
+// check if it has response
+if ($title -> response) {
+    // get data
+    echo $title -> data -> getTitle() . ' (' . $title -> data -> getYear() . ')';
+}
+echo "<pre>";
+print_r($title);
---
+++ examples/getSerieEpisode.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . "/../src/loader.php";
+
+$api = new BertMaurau\OMDB\Core\API("3077118c");
+
+
+$title = (new BertMaurau\OMDB\Models\Serie) -> getByImdbId("tt0944947");
+
+// check if it has response
+if ($title -> response) {
+
+    // get data
+    echo $title -> data -> getTitle() . ' (' . $title -> data -> getYear() . ')';
+
+    // get random episode
+    if ($title -> type === 'series') {
+        $episode = $title -> data -> getEpisodeForSeason(3, 4);
+    }
+}
+echo "<pre>";
+print_r($episode);
