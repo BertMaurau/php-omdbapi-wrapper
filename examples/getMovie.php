b69b32527846a8854f2cf82e890503f0bdbc3781
Bert Maurau <hello@bertmaurau.be>
2018-08-14 22:22:01 +0200

    added example to get by title, year and type

--- examples/getMovie.php
+++ examples/getMovie.php
@@ -5,7 +5,9 @@ require_once __DIR__ . "/../src/loader.php";
 $api = new BertMaurau\OMDB\Core\API("3077118c");
 
 
-$title = (new BertMaurau\OMDB\Models\Title) -> getByImdbId("tt2085059");
+// $title = (new BertMaurau\OMDB\Models\Title) -> getByImdbId("tt2085059");
+
+$title = (new BertMaurau\OMDB\Models\Title) -> getByTitle("Total Recall", 1990, "movie");
 
 // check if it has response
 if ($title -> response) {
